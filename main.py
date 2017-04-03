3d14f5504abb6bace0a5c2f2d9716b511c06f534
0x4n6 <bertdewit@gmail.com>
2017-04-03 17:55:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-import aes
 import os
+from utils import new
 
 def main(data):
     key_128 = os.urandom(16)
     IV = os.urandom(16)
-    ecb_mode = AES.ECB_MODE
-    cbc_mode = AES.CBC_MODE
 
     print "Input: {}, {} bytes".format(data, len(data))
     print "Key: {}, {} bytes".format(key_128, len(key_128))
     print "IV: {}, {} bytes".format(IV, len(IV))
 
-    encryptor = AES.new(key_128, cbc_mode, IV)
+    encryptor = new(key_128, cbc_mode, IV)
     encrypted = encryptor.encrypt(data)
     print "Encrypted: {}, {} bytes".format(encrypted, len(encrypted))
     #print "Hex: " + encrypted.encode('hex')
 
-    decryptor = AES.new(key_128, cbc_mode, IV)
+    decryptor = new(key_128, cbc_mode, IV)
     decrypted = decryptor.decrypt(encrypted)
     print "Decrypted: {}, {}".format(decrypted, len(decrypted))
     #print "Hex: " + decrypted.encode('hex')
