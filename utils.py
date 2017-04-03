2481557942db17336e0af2d5b995b53abe272e12
0x4n6 <bertdewit@gmail.com>
2017-04-03 18:03:56 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,7 @@ from array import array
 
 def new(key, IV=None):
 	if IV is None:
-        	raise ValueError
-			
+        	raise ValueError	
 	return CBCMode(AES(key), IV)
     
 def galois_multiply(a, b):
